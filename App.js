af839db3a4f6787aee50db24c5a592f7ab4c3128
Guilherme Henrique Couto de Jesus <guicoutoo@outlook.com>
2022-12-13 16:09:51 -0300

    [x]feat:add novos dados ao mock

--- App.js
+++ App.js
@@ -1,24 +1,44 @@
-import { SafeAreaView, StatusBar, View } from "react-native";
-import Cesta from "./src/Screen/Cesta";
 import {
-  useFonts,
   Montserrat_400Regular,
   Montserrat_700Bold,
+  useFonts,
 } from "@expo-google-fonts/montserrat";
+import { SafeAreaView, StatusBar, View } from "react-native";
 import cesta from "./src/components/mocks";
+import Cesta from "./src/Screen/Cesta";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback, useEffect, useState } from "react";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     MontserratRegular: Montserrat_400Regular,
     MontserratBold: Montserrat_700Bold,
   });
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+      } catch (e) {
+        console.warn(e);
+      }
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
 
   if (!fontsLoaded) {
-    return <View />;
+    return null;
   }
 
   return (
-    <SafeAreaView>
+    <SafeAreaView onLayout={onLayoutRootView}>
       <StatusBar />
       <Cesta {...cesta} />
     </SafeAreaView>
